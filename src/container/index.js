a5c07f5eb72d5df69d0d0a04fad8749d20376183
TanyaRoot <kofeneformat@gmail.com>
2018-05-25 18:59:45 +0300

    day6: callback, start loggin

--- src/container/index.js
+++ src/container/index.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
 import Header from './header/index'
 import Calendar from './calendar/dayPicker'
+import UserPage from './userPage/userPage'
 
 class Container extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      profilStatus: false
+    }
+  }
+
+  getProfilStatus = (status) => {
+    this.setState({profilStatus: status})
+    // console.log('status', status);
+  }
+
   render() {
     return (
       <div className='123'>
-        <Header />
-        <Calendar />
+        <Header getProfilStatus={this.getProfilStatus} status={this.state.profilStatus}/>
+        {this.state.profilStatus
+           ?
+            <UserPage getProfilStatus={this.getProfilStatus} status={this.state.profilStatus} />
+           :
+           <Calendar />
+         }
       </div>
     )
   }
